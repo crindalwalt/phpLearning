e1ef51176d825109556841269f46ee7033a1b15e
Shahzad Farooq <crindalwalt@protonmail.com>
2023-03-18 20:21:09 +0500

    operator and ifelse done

---
+++ 1-operator.php
@@ -0,0 +1,43 @@
+<?php
+
+echo "helo world";
+echo "<br>";
+echo "<br>";
+$num1 = 9;
+
+
+$num2 = 10;
+$condition = !(5 < 2 and 3 > 2);
+// echo $num1 + $num2;
+echo '<br><br>';
+
+echo var_dump($condition);
+echo '<br><br>';
+
+
+
+// echo var_dump($num1 ** $num2);
+
+
+/*
+ * OPERATORS IN PHP
+ *      -> Arithmatic operator
+ *      + ,-,*,/,%,**
+ * 
+ * 
+ *      -> camparision operator
+ *      >,<,>=,<=,==,===,!=
+ * 
+ * 
+ *      -> Assignment operator
+ *      =,+=,-=,*=,/=,%=,
+ * 
+ * 
+ *      -> Logical operator
+ *          or ,and ,!
+ *  
+ * 
+ * 
+ * 
+ * 
+ */
---
+++ 2-ifElse.php
@@ -0,0 +1,35 @@
+<?php
+
+// if ($num1 < $num2) {
+//     echo "num1 is smaller then num2 ";
+//     if($num1 <10){
+//         echo "num1 is even smaller then 10";
+//     }
+// }else if($num1 == 34){
+    
+    
+// }else if($um2 == "alkdjf"){
+
+// }else{
+//     echo "num1 is greater then num1 ";
+//     if($num2 == "22"){
+//         echo "kldsjf;";
+//     }else{
+//         echo "lkdjf;alkd";
+//     }
+// }
+
+
+$age = "12";
+
+
+if($age <18 and $age >0){
+    echo "you cannot consume alcohol";
+}else if($age < 25){
+    echo "you can consume captain not gold leaf";
+}else if($age > 25){
+    echo "bhai tu jo merzi pi bhai me ja";
+}else{
+    echo "cigerate noshi haram shud";
+}
+echo '<br><br>';
